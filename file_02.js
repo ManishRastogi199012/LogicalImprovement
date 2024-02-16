db2ff8b1ac6d265aef763af9a5f0bd53f0969f53
ManishRastogi199012 <68321046+ManishRastogi199012@users.noreply.github.com>
2024-02-16 13:57:38 +0530

    min max from array

---
+++ file_02.js
@@ -0,0 +1,26 @@
+/*
+    Find the maximum number in array
+*/
+
+a= [-2,-5,-33,0,-56];
+maxNumber=0;
+minNumber=0;
+sumOfElement = 0;
+for(let i= 0;i<a.length-1;i++){
+    // console.log(a[i])
+    // console.log(a[i+1])
+    maxNumber = a[i]<a[i+1] ? a[i+1] : a[i]
+    minNumber = a[i]>a[i+1] ? a[i+1] : a[i]
+    // if(a[i]<a[i+1]){
+    //     maxNumber=a[i]
+    // }
+    // else{
+    //     maxNumber=a[i+1]
+    // }
+    // sumOfElement = sumOfElement+a[i]
+}
+c= Math.max(...a)
+c= Math.max(...a)
+console.log(c)
+// console.log(maxNumber)
+// console.log(sumOfElement)
